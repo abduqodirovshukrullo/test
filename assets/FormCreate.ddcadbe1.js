438cbfe7029f0ee481a6cdc9cbb788c258a52a43
Shukrullo Abduqodirov <abduqodirov0101@gmail.com>
2023-04-18 21:06:12 +0500

    ++

---
+++ assets/FormCreate.ddcadbe1.js
@@ -0,0 +1,1 @@
+import{y as r}from"./index.ccf75e8b.js";const e={};function t(c,n){return null}const _=r(e,[["render",t]]);export{_ as default};
---
+++ assets/list.3b7400f9.js
@@ -0,0 +1,1 @@
+import{y as r}from"./index.ccf75e8b.js";const t={};function c(e,n){return null}const _=r(t,[["render",c]]);export{_ as default};
